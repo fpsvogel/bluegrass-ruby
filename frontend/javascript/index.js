40f8b3ae214cc0b76d5fb950bdd4eb5f401d64bc
Jos O'shea <jos@joshea.dev>
2023-05-05 01:30:30 -0400

    update

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -6,28 +6,25 @@ import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
 console.info("Bridgetown is loaded!")
 
-  function updateNextThirdTuesday() {
+  function updateThirdTuesday() {
     const today = new Date();
-    let thirdTuesday = new Date(today.getFullYear(), today.getMonth(), 1);
-      
-    while (thirdTuesday.getDay() !== 2) {
-      thirdTuesday.setDate(thirdTuesday.getDate() + 1);
+    const thirdTueday = new Date(today.getFullYear(), today.getMonth(), 14);
+  
+    while (thirdTueday.getDay() !== 2) {
+      thirdTueday.setDate(thirdTueday.getDate() + 1);
     }
   
-    thirdTuesday.setDate(thirdTuesday.getDate() + 14);
+    if (today.getMonth() === thirdTueday.getMonth() && today.getDate() >= thirdTueday.getDate())  {
+      thirdTueday.setMonth(thirdTueday.getMonth() + 1);
+    } else {
   
-    if (today >= thirdTuesday) {
-      const nextMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1);
-      thirdTuesday = updateNextThirdTuesday.call(nextMonth);
     }
-      
+  
     const options = { weekday: 'long', month: 'long', day: 'numeric' };
-    const formattedDate = today.getMonth() === thirdTuesday.getMonth() ? today.toLocaleDateString('en-US', options) : thirdTuesday.toLocaleDateString('en-US', options);
-    
+    const formattedDate = thirdTueday.toLocaleDateString('en-US', options);
+  
     const meetingDate = document.getElementById('meeting-date');
     meetingDate.textContent = formattedDate;
-    
-    return thirdTuesday;
   }
-  
-  updateNextThirdTuesday();
+
+  updateThirdTuesday();
